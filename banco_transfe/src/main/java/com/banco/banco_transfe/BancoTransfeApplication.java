3fe1661040e5580ef9dcdc6a15c6192b4249ab85
Matheus Kennedy <matheuskennedyt@gmail.com>
2024-01-27 07:51:51 -0300

    feat: primeiras configurações

---
+++ banco_transfe/src/main/java/com/banco/banco_transfe/BancoTransfeApplication.java
@@ -0,0 +1,13 @@
+package com.banco.banco_transfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoTransfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoTransfeApplication.class, args);
+	}
+
+}
---
+++ banco_transfe/src/test/java/com/banco/banco_transfe/BancoTransfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.banco_transfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTransfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
